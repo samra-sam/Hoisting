f38df74fd675816073ead04696c29e9f629773c8
samra <samraabdulhameed7@gmail.com>
2024-05-23 14:48:02 +0500

    Created Hoisting in javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log(string);
+var string = "sam";
+console.log(string, "\n");
+console.log(number);
+var number = 25;
+console.log(number, "\n");
+console.log(x);
+var x = 4;
+console.log(x, "\n");
+console.log(y);
+var y = "samra";
+console.log(y, "\n");
+console.log(z);
+var z = 2 * 8;
+console.log(z);
